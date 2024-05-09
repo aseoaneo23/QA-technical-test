b8e4267b9d2a620cdc1496c9260c690755923dae
aseoaneo23 <aseoaneo22@fpcoruna.afundacion.org>
2024-05-09 15:04:09 +0200

    Readme actualizado y cambiada config en cypress.config.js acorde a nuevos directorios

--- Prueba_automatización/cypress.config.js
+++ Prueba_automatización/cypress.config.js
@@ -4,7 +4,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      return require("../cypress/plugins/index.js")(on, config);
+      return require("./cypress/plugins/index.js")(on, config);
     },
     specPattern: "cypress/features/**/*.feature",
     baseUrl: "https://www.google.com",
